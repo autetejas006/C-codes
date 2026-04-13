962119a0f0407646f238a3ed0308b3fad0ed5381
autetejas006 <autetejas006@gmail.com>
2026-04-13 14:47:41 +0530

    pro013

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = 123, rev = 0;
+        while (n != 0)
+        {
+            rev = rev * 10 + n % 10;
+            n /= 10;
+        }
+        Console.WriteLine(rev);
+    }
+}
