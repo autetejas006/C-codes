8c55f2a2530e16423e2b10dc8cc6f8a595670612
autetejas006 <autetejas006@gmail.com>
2026-04-13 14:42:21 +0530

    pro011

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = 5, fact = 1;
+        for (int i = 1; i <= n; i++) fact *= i;
+        Console.WriteLine(fact);
+    }
+}
