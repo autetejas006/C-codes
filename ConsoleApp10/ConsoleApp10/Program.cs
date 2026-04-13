2de972f24847ea5ecc8aaa94d0b8e6d7b14dd512
autetejas006 <autetejas006@gmail.com>
2026-04-13 14:35:26 +0530

    pro010

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,13 @@
+﻿
+ using System;
+class Program
+{
+    static void Main()
+    {
+        int a = 5, b = 10;
+        int temp = a;
+        a = b;
+        b = temp;
+        Console.WriteLine(a + " " + b);
+    }
+}
